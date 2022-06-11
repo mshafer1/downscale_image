e570b52d72c18805ca5a71e48414635957c6ddf7
Matthew <2565361+mshafer1@users.noreply.github.com>
2022-06-11 09:36:25 -0500

    add error handling and testing

--- downscale_image/__main__.py
+++ downscale_image/__main__.py
@@ -1,19 +1,28 @@
 import pathlib
+import sys
 import click
 
 import downscale_image
 
 
 @click.command()
-@click.option("--max-size", default=2, help="Max output size (in MB)")
+@click.option("--max-size", default=2, help="Max output size (in MB)", type=click.IntRange(min=0, min_open=True))
 @click.argument("in_file", type=click.Path())
 def main(max_size, in_file):
     in_file = pathlib.Path(in_file)
 
     print(f"Downscaling {in_file}...")
-    downscale_image.downscale(in_file, max_mega_bytes=max_size)
-    print(f"Finished")
+    try:
+        downscale_image.downscale(in_file, max_mega_bytes=max_size)
+        print(f"Finished")
+    except Exception as e:
+        print("An error occured", file=sys.stderr)
+        print(e, file=sys.stderr)
+        print("")
+        print("")
+        input("Press enter to continue...")
+        click.Abort(e)
 
 
-if __name__ == "__main__":
+if __name__ == "__main__":  # pragma: no cover
     main()
--- tests/test_downscale.py
+++ tests/test_downscale.py
@@ -1,6 +1,6 @@
 import pathlib
-import downscale_image
-from typing import NamedTuple
+from typing import Callable, List, NamedTuple
+import io
 
 import pytest
 from pytest_snapshot.plugin import Snapshot
@@ -39,3 +39,30 @@ def test___image___downscale___outputs_expected_file(test_result: _ResultInfo, s
 def test___image___downscale___output_is_request_size(test_result: _ResultInfo):
     out_file_size = len(test_result.output.read_bytes()) / 1024 / 1024
     assert out_file_size < test_result.max_mb
+
+
+def test___invalid_path___downscale___prints_message(
+    runner: Callable[[List[str]], click.testing.Result],
+    tmp_path: pathlib.Path,
+    monkeypatch,
+):
+    monkeypatch.setattr('sys.stdin', io.StringIO('\n'*5))
+    result = runner([str(tmp_path), "--max-size=-1"])
+
+    assert result.exception is not None
+    assert "An error occured" in str(result.output)
+
+
+def test___target_already_exists___downscale___prints_message(
+    runner: Callable[[List[str]], click.testing.Result],
+    tmp_path: pathlib.Path,
+):
+    file = tmp_path / "image.png"
+    file.touch()
+    out_file = tmp_path / "image_smaller.png"
+    out_file.touch()
+
+    result = runner([str(file)])
+
+    assert result.exception is not None
+    assert "An error occured" in str(result.output)
