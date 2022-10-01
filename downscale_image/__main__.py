e92aa0f83fca323235d464fb60eb9d88241e5bf1
Matthew Shafer (laptop) <mshafer1@harding.edu>
2022-10-01 14:28:09 -0500

    setup to do all files in directory

--- downscale_image/__main__.py
+++ downscale_image/__main__.py
@@ -4,6 +4,7 @@ import sys
 import platform
 
 import click
+import pathspec
 
 import downscale_image
 
@@ -12,6 +13,7 @@ _ON_WINDOWS = platform.system().lower() == "windows"
 if _ON_WINDOWS:
     from downscale_image import _registry_utils
 
+_DEFAULT_MATCHES = ["!.venv/", "!.git/", "!objects/"] + [f"*{ext}" for ext in downscale_image.SUPPORTED_FILE_EXTENSIONS]
 
 @click.command()
 @click.option(
@@ -27,9 +29,9 @@ if _ON_WINDOWS:
     is_flag=True,
     default=False,
 )
-@click.argument("in_file", type=click.Path(exists=True, dir_okay=False))
+@click.argument("in_file", metavar="FILE_OR_DIRECTORY", type=click.Path(exists=True, dir_okay=True))
 def main(max_size, in_file, add_to_right_click_menu: bool):
-    """Downscale in_file to desired max-size."""
+    """Downscale file_or_directory to desired max-size."""
     if add_to_right_click_menu:
         if not _ON_WINDOWS:
             raise Exception("Error, registry right click menus are only support on Windows.")
@@ -39,15 +41,31 @@ def main(max_size, in_file, add_to_right_click_menu: bool):
 
     in_file = pathlib.Path(in_file)
 
-    print(f"Downscaling {in_file}...")
-    try:
-        downscale_image.downscale(in_file, max_mega_bytes=max_size)
-        print(f"Finished")
-    except Exception as e:
-        print("An error occured", file=sys.stderr)
-        print(e, file=sys.stderr)
-        print("")
-        print("")
+    files_to_prcoess = []
+
+    if in_file.is_dir():
+        spec = pathspec.PathSpec.from_lines(pathspec.patterns.GitWildMatchPattern, _DEFAULT_MATCHES)
+        files_to_prcoess = [pathlib.Path(p) for p in spec.match_tree(in_file)]
+    else:
+        files_to_prcoess = [in_file]
+
+    fail_count = 0
+    for file in files_to_prcoess:
+        print(f"Downscaling {file}...")
+        try:
+            downscale_image.downscale(file, max_mega_bytes=max_size)
+            print(f"Finished")
+        except Exception as e:
+            fail_count += 1
+            if fail_count > 5:
+                print("Several errors have occured, stopping")
+                break
+            print("An error occured", file=sys.stderr)
+            print(e, file=sys.stderr)
+            print("")
+            print("")
+    if fail_count > 0:
+        print("See above for errors")
         input("Press enter to continue...")
         click.Abort(e)
 
