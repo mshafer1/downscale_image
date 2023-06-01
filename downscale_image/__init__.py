2c4fcc6116d749adb204a4d2a94f9bd7655b4a71
Matthew Shafer (laptop) <mshafer1@harding.edu>
2023-05-31 19:51:59 -0500

    better handle beyond .1 size of output

--- downscale_image/__init__.py
+++ downscale_image/__init__.py
@@ -21,6 +21,24 @@ def _get_file_size_in_mega(file: pathlib.Path) -> float:
     file_size = os.path.getsize(file)
     return _bytes_to_mega_bytes(file_size)
 
+def _decrements():
+    """
+    >>> t = _decrements(); list([next(t) for _ in range(15)])
+    [1.0, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1, 0.09, 0.08, 0.07, 0.06, 0.05]
+
+    >>> t = _decrements(); _ = [next(t) for _ in range(15)]; list([next(t) for _ in range(10)])
+    [0.04, 0.03, 0.02, 0.01, 0.009, 0.008, 0.007, 0.006, 0.005, 0.004]
+    """
+    step_size = 1
+    current_value = 1.0
+    yield current_value
+    while True: # iterate forever
+        step = 10**(-step_size)
+        if current_value - step <= 0:
+            step_size += 1
+            step = 10**(-step_size)
+        current_value = round(current_value- step, step_size)
+        yield round(current_value, 3)
 
 def _scale(img: pathlib.Path, out_img: pathlib.Path, scale: float) -> pathlib.Path:
     """Use ffmpeg to scale the image by the desired amount."""
@@ -48,8 +66,8 @@ def downscale(
     """Incrementally downscale img until it is <= max_mega_bytes in size."""
     current_size = _get_file_size_in_mega(img)
     working_img = img
-    current_scale = 1.0
-    current_multiplier = 10
+    scales = _decrements()
+    current_scale = next(scales)
 
     if current_size <= max_mega_bytes:
         print("Not changing")
@@ -71,9 +89,6 @@ def downscale(
         working_img = _scale(img, out_img=working_img, scale=current_scale)
 
         current_size = _get_file_size_in_mega(working_img)
-        current_scale -= 1 / current_multiplier
-        if current_scale < 0:
-            current_scale = 1/current_multiplier
-            current_multiplier += 10
+        current_scale = next(scales)
 
     return working_img
